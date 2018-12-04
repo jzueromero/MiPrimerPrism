842a30cc5779c40e3be855102d297d63d4b730ae
Josue Emmanuel Romero Alfaro <josue.romero@sv.cds>
2018-12-04 14:58:05 -0600

    Siguiendo post de xamgirl, a probar navegacion

---
+++ MiPrimerPrism/MiPrimerPrism/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using MiPrimerPrism.ViewModels;
+using MiPrimerPrism.Views;
+using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace MiPrimerPrism
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+        }
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<HomePage, HomePageViewModel>();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ MiPrimerPrism/MiPrimerPrism/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiPrimerPrism.ViewModels
+{
+    
+    public class HomePageViewModel
+    {
+        INavigationService _navigationService;
+        public HomePageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+    }
+}
